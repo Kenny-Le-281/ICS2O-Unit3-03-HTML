80daed0d57ff2426af9823aa5682c0421e374f8c
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-04-04 17:18:59 +0000

    Added calculations in script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@
  * Created on: Mar 2022
  * This file contains the JS functions for index.html 
 */
+
+'use strict'
+
+/**
+ * This function calculates the volume of the sphere.
+ */
+function calculate () {
+  // input
+  const radius = parseFloat(document.getElementById('sphere-radius').value)
+
+  // process
+  const volume = 4 / 3 * Math.PI * Math.pow(radius, 3)
+
+  // output
+  document.getElementById('volume').innerHTML = 'The volume of the sphere is: ' + volume.toFixed(2) + ' cm³'
+}
